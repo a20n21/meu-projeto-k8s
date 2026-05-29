3648fa0d4434e32d620c31b83e0f578f6e9c7288
a20n21 <andersonmatias188@outlook.com>
2026-05-29 21:43:21 +0000

    fix: adicionando rota raiz para satisfazer a startup probe do k8s

--- backend/server.js
+++ backend/server.js
@@ -2,19 +2,22 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
-// Middleware para entender requisições JSON
 app.use(express.json());
 
-// "Banco de Dados" temporário na memória RAM
+// BANCO DE DADOS EM MEMÓRIA
 let funcionarios = [
     { id: 1, nome: "Matias", cargo: "DevOps Engineer", departamento: "Tecnologia" },
     { id: 2, nome: "Ana Silva", cargo: "Recrutadora Tech", departamento: "Recursos Humanos" }
 ];
 
-// Rota de Login (Simulada)
+// MÁGICA AQUI: Rota raiz para o Kubernetes Probes (Startup/Liveness/Readiness) não darem 404
+app.get('/', (req, res) => {
+    res.status(200).json({ status: "ok", message: "Healthcheck da raiz respondendo!" });
+});
+
+// Rota de Login
 app.post('/api/login', (req, res) => {
     const { usuario, senha } = req.body;
-
     if (usuario === 'admin' && senha === 'matias123') {
         return res.json({ success: true, token: "fake-jwt-token-rh-2026", mensagem: "Login efetuado com sucesso!" });
     } else {
@@ -30,23 +33,14 @@ app.get('/api/funcionarios', (req, res) => {
 // Rota para Cadastrar Funcionário
 app.post('/api/funcionarios', (req, res) => {
     const { nome, cargo, departamento } = req.body;
-    
     if (!nome || !cargo || !departamento) {
         return res.status(400).json({ mensagem: "Todos os campos são obrigatórios." });
     }
-
-    const novoFuncionario = {
-        id: funcionarios.length + 1,
-        nome,
-        cargo,
-        departamento
-    };
-
+    const novoFuncionario = { id: funcionarios.length + 1, nome, cargo, departamento };
     funcionarios.push(novoFuncionario);
     res.status(201).json({ mensagem: "Funcionário cadastrado com sucesso!", funcionario: novoFuncionario });
 });
 
-// Rota padrão para o Ingress não quebrar na raiz da API
 app.get('/api', (req, res) => {
     res.json({ status: "alive", app: "PeopleFlow API" });
 });
